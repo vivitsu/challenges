2500d20741e103d3c3df5c9d9fd94b3ed852e521
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-06-26 19:56:07 -0700

    Add solutions to problems 4 and 29

---
+++ projecteuler/1-9/4.hs
@@ -0,0 +1,13 @@
+intToList :: Int -> [Int]
+intToList = reverse . helper
+    where helper 0 = []
+          helper x = (x `rem` 10) : helper (x `div` 10)
+
+isPalindrome :: Int -> Bool
+isPalindrome x
+    | xs == reverse xs = True
+    | otherwise = False
+  where xs = intToList x
+
+largestPalindrome :: Int
+largestPalindrome = maximum $ filter isPalindrome [x*y | x <- [100..999], y <- [100..999]]
