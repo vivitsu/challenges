6d667e5eaafcd66a60eec9030ddf43a4ab574e34
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2016-11-11 12:16:54 -0800

    Better solution to LeapYear.hs

--- exercism/haskell/leap/src/LeapYear.hs
+++ exercism/haskell/leap/src/LeapYear.hs
@@ -1,25 +1,8 @@
 module LeapYear (isLeapYear) where
 
-isDivisibleBy4 :: Integer -> Bool
-isDivisibleBy4 year = if f == 0 then True else False
-  where f = fromIntegral $ year `mod` 4
-
-isNotDivisibleBy100 :: Integer -> Bool
-isNotDivisibleBy100 year = if f == 0 then False else True
-  where f = fromIntegral $ year `mod` 100
-
-isDivisibleBy400 :: Integer -> Bool
-isDivisibleBy400 year = if f == 0 then True else False
-  where f = fromIntegral $ year `mod` 400
-
 isLeapYear :: Integer -> Bool
-isLeapYear year = if isDivisibleBy4 year then
-                    if isNotDivisibleBy100 year then
-                      True
-                    else
-                      if isDivisibleBy400 year then
-                        True
-                      else
-                        False
-                  else
-                    False
+isLeapYear year | year `isDivisibleBy` 400 = True
+                | year `isDivisibleBy` 100 = False
+                | year `isDivisibleBy` 4 = True
+                | otherwise = False
+                where isDivisibleBy year num = year `mod` num == 0
